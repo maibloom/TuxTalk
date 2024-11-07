81d77ababc891b4a825b6f9458324fe67e2c7199
devtracer <devoft1@gmail.com>
2024-11-08 01:35:03 +0330

    Update Installer.sh

--- Installer.sh
+++ Installer.sh
@@ -3,10 +3,13 @@ pip install pywebview
 pip install pyinstaller
 
 # Build the executable
-sudo pyinstaller --onefile --name "TuxTalk" --distpath /usr/local/bin Tuxtalk.py
+sudo pyinstaller --onefile --name "TuxTalk" --distpath /usr/local/bin/Tux-talk Tuxtalk.py
 
 # Move the .desktop file to the right location
 mv tux-talk.desktop ~/.local/share/applications/
+
+# Move the logo too
+mv logo.png /usr/local/bin/Tux-talk
 
 # Make the .desktop file executable
 chmod +x ~/.local/share/applications/tux-talk.desktop

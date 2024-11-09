8276e351c234d6ac4d52bcd246d5f48284d9257e
devtracer <devoft1@gmail.com>
2024-11-09 08:18:08 +0000

    Update Installer.sh

--- Installer.sh
+++ Installer.sh
@@ -1,9 +1,14 @@
-# Install dependencies
+# Install pip
 sudo pacman -S python-pip
+
+# Install dependencies
+sudo pacman -Sy pkg-config gtk3 webkit2gtk
+
+# Install Python packages
 pip install pywebview
 pip install pyinstaller
 pip install requests
-pip install screenview
+pip install screeninfo
 
 # Build the executable
 sudo pyinstaller --onefile --name "TuxTalk" --distpath /usr/local/bin/Tux-talk Tuxtalk.py

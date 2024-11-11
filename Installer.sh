3749b9a7b9acf12ca7fe5f235fa8632dd50b7025
devtracer <devoft1@gmail.com>
2024-11-11 20:03:40 +0330

    Update Installer.sh

--- Installer.sh
+++ Installer.sh
@@ -1,23 +1,11 @@
-# Install pip
-sudo pacman -S python-pip
+# Move the TuxTalk directory to /usr/bin
+mv TuxTalk /usr/bin/
 
-# Install dependencies
-sudo pacman -Sy pkg-config gtk3 webkit2gtk
+# Compile the C program and place the binary in /usr/bin/TuxTalk
+gcc /usr/bin/TuxTalk/my_program.c -lcurl -lwebview -lX11 -o /usr/bin/TuxTalk/TuxTalk
 
-# Install Python packages
-pip install pywebview
-pip install pyinstaller
-pip install requests
-pip install screeninfo
+# Set executable permissions for the compiled binary
+chmod +x /usr/bin/TuxTalk/TuxTalk
 
-# Build the executable
-sudo pyinstaller --onefile --name "TuxTalk" --distpath /usr/local/bin/Tux-talk Tuxtalk.py
-
-# Move the .desktop file to the right location
-mv tux-talk.desktop ~/.local/share/applications/
-
-# Move the logo too
-mv logo.png /usr/local/bin/Tux-talk
-
-# Make the .desktop file executable
-chmod +x ~/.local/share/applications/tux-talk.desktop
+# Copy the .desktop file to /usr/share/applications to make it accessible in the application menu
+cp /usr/bin/TuxTalk/TuxTalk.desktop /usr/share/applications/

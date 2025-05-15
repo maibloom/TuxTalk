03b1fabb0f0f0f061bbdf689622488f3fc0478e7
Amirali Dashti <devoft1@gmail.com>
2025-05-16 02:43:20 +0330

    Update TuxTalkInstall.sh

--- v1.0.0/TuxTalkInstall.sh
+++ v1.0.0/TuxTalkInstall.sh
@@ -1,10 +1,12 @@
-sudo mkdir /usr/bin/TuxTalk
+sudo mkdir -p /usr/bin/TuxTalk
+sudo mkdir -p /usr/share/applications/
 
 chmod +x TuxTalk.desktop
 chmod +x TuxTalk.py
 
-sudo cp TuxTalk.py /usr/bin/TuxTalk/ && sudo cp TuxTalk.png /usr/bin/TuxTalk/
+sudo cp TuxTalk.py /usr/bin/TuxTalk/
+sudo cp TuxTalk.png /usr/bin/TuxTalk/
+sudo cp TuxTalk.desktop /usr/share/applications/
+sudo update-desktop-database /usr/share/applications/
 
-cp TuxTalk.desktop ~/.local/share/applications/
-
-update-desktop-database ~/.local/share/applications
+echo "Installation completed!"
